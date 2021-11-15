fe2069a53d4501acd2f26c377e99cc52a4d0edcd
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-11-15 09:14:36 +0530

    Fixing up stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 from autoclear.__init__ import __version__
 
 setup(
-    name="autoclear",
+    name='autoclear',
     version=__version__,
-    
+    description='Automatically clear the terminal screen',
+    author='Nandagopal Menon',
+    author_email="Nandagopalnmenon@gmail.com",
+    url="#",
+    packages=['autoclear'],
+    license="MIT",
 )
